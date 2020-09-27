107a0230a679a84f3a27cbfa081d94e519b88ef3
Kerby-vlad <53996780+Kerby-vlad@users.noreply.github.com>
2020-09-28 00:01:34 +0300

    26.09   function loadOffers     added functions to load cards

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 'use strict';
 
 const ROOM_TYPE = [`palace`, `flat`, `house`, `bungalow`];
+const ROOM_TYPE_TRANSLATER = {
+  palace: `Дворец`,
+  flat: `Квартира`,
+  house: `Дом`,
+  bungalow: `Бунгало`
+
+};
 const FEATURES_LIST = [`wifi`, `dishwasher`, `parking`, `washer`, `elevator`, `conditioner`];
 const PHOTOS_LIST = [`http://o0.github.io/assets/images/tokyo/hotel1.jpg`, `http://o0.github.io/assets/images/tokyo/hotel2.jpg`, `http://o0.github.io/assets/images/tokyo/hotel3.jpg`];
 const MAP = document.querySelector(`.map`);
 const PIN_TEMPLATE = document.querySelector(`#pin`).content.querySelector(`.map__pin`);
+const CARD_TEMPLATE = document.querySelector(`#card`).content.querySelector(`.map__card`);
+let CARD_PHOTO_TEMPLATE = document.querySelector(`#card`).content.querySelector(`.popup__photo`);
 const PIN_CONTAINER = document.querySelector(`.map__pins`);
 const START_CHECKIN = 12;
 const OCLOCK = `:00`;
 const PIN_WIDTH = 40;
 const PIN_LOCATION_Y_START = 130;
 let ads = [];
-let features;
-let photos;
-let fragments = [];
+let featuresArr;
+let featuresWrapper;
+let photosArr;
+let photosWrapper;
+let pinFragments = [];
+let cardsFragments = [];
 
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
@@ -32,8 +44,8 @@ function getRandomArr(arr, plotArr, length, max) {
 
 function fillOffers() {
   for (let i = 1; i <= 8; i++) {
-    features = [];
-    photos = [];
+    featuresArr = [];
+    photosArr = [];
     let obj = {
       author: {
         avatar: `img/avatars/user0${i}.png`
@@ -43,13 +55,13 @@ function fillOffers() {
         address: `${getRandomInt(600)},${getRandomInt(350)}`,
         price: getRandomInt(1000),
         type: ROOM_TYPE[i % ROOM_TYPE.length],
-        rooms: getRandomInt(4),
+        rooms: getRandomInt(4) + 1,
         guests: getRandomInt(3),
-        checkin: `${START_CHECKIN + getRandomInt(2)}+${OCLOCK}`,
-        checkout: `${START_CHECKIN + getRandomInt(2)}+${OCLOCK}`,
-        features: getRandomArr(features, FEATURES_LIST, getRandomInt(FEATURES_LIST.length), FEATURES_LIST.length),
+        checkin: `${START_CHECKIN + getRandomInt(2)}${OCLOCK}`,
+        checkout: `${START_CHECKIN + getRandomInt(2)}${OCLOCK}`,
+        features: getRandomArr(featuresArr, FEATURES_LIST, getRandomInt(FEATURES_LIST.length), FEATURES_LIST.length),
         description: `desc${i}`,
-        photos: getRandomArr(photos, PHOTOS_LIST, getRandomInt(PHOTOS_LIST.length), PHOTOS_LIST.length)
+        photos: getRandomArr(photosArr, PHOTOS_LIST, getRandomInt(PHOTOS_LIST.length), PHOTOS_LIST.length)
       },
       location: {
         x: getRandomInt(MAP.offsetWidth - PIN_WIDTH),
@@ -60,27 +72,83 @@ function fillOffers() {
   }
 }
 
-function fillTemplate() {
+function fillPinTemplate() {
   for (let i = 0; i < ads.length; i++) {
     let fragment = PIN_TEMPLATE.cloneNode(true);
     fragment.setAttribute(`style`, `left: ${ads[i].location.x + PIN_WIDTH / 2}px; top: ${ads[i].location.y + PIN_WIDTH / 2}px`);
     fragment.querySelector(`img`).src = ads[i].author.avatar;
     fragment.querySelector(`img`).alt = ads[i].offer.title;
-    fragments.push(fragment);
+    pinFragments.push(fragment);
+  }
+}
+
+function fillCardList(wrapper, objectKey, adItem, elementTemplate) {
+  for (let j = 0; j < adItem.offer[objectKey].length; j++) {
+    let element;
+    if (objectKey === `features`) {
+      element = document.createElement(`li`);
+      element.className = `popup__feature popup__feature--${adItem.offer[objectKey][j]}`;
+    } else {
+      element = elementTemplate.cloneNode(true);
+      element.src = adItem.offer[objectKey][j];
+    }
+    wrapper.appendChild(element);
+  }
+}
+
+function refactorLists(fragment) {
+  featuresWrapper = fragment.querySelector(`.popup__features`);
+  featuresWrapper.innerHTML = ``;
+  photosWrapper = fragment.querySelector(`.popup__photos`);
+  photosWrapper.removeChild(photosWrapper.querySelector(`.popup__photo`));
+}
+
+function fillCardTemplate() {
+  for (let i = 0; i < ads.length; i++) {
+    let fragment = CARD_TEMPLATE.cloneNode(true);
+    fragment.querySelector(`.popup__title`).textContent = ads[i].offer.title;
+    fragment.querySelector(`.popup__text--address`).textContent = ads[i].offer.address;
+    fragment.querySelector(`.popup__text--price`).textContent = `${ads[i].offer.price} ₽/ночь`;
+    fragment.querySelector(`.popup__type`).textContent = `${ROOM_TYPE_TRANSLATER[ads[i].offer.type]}`;
+    fragment.querySelector(`.popup__text--capacity`).textContent = `${ads[i].offer.rooms} комнаты для ${ads[i].offer.guests} гостей`;
+    fragment.querySelector(`.popup__text--time`).textContent = `Заезд после ${ads[i].offer.checkin}, выезд\t до ${ads[i].offer.checkout}`;
+    fragment.querySelector(`.popup__description `).textContent = ads[i].offer.description;
+    fragment.querySelector(`.popup__description `).textContent = ads[i].offer.description;
+    fragment.querySelector(`.popup__avatar`).src = ads[i].author.avatar;
+    refactorLists(fragment);
+    fillCardList(featuresWrapper, `features`, ads[i], null, `.popup__feature`);
+    fillCardList(photosWrapper, `photos`, ads[i], CARD_PHOTO_TEMPLATE);
+    cardsFragments.push(fragment);
   }
 }
 
 function showPins() {
-  for (let i = 0; i < fragments.length; i++) {
-    PIN_CONTAINER.appendChild(fragments[i]);
+  for (let i = 0; i < pinFragments.length; i++) {
+    PIN_CONTAINER.appendChild(pinFragments[i]);
   }
 }
 
 function loadOffers() {
   MAP.classList.remove(`map--faded`);
   fillOffers();
-  fillTemplate();
+  loadPins();
+  loadCards();
+}
+
+function loadPins() {
+  fillPinTemplate();
   showPins();
+}
+
+function loadCards() {
+  fillCardTemplate();
+  showCards();
+}
+
+function showCards() {
+  cardsFragments.forEach(function (card) {
+    document.querySelector(`.map__filters-container`).insertAdjacentHTML(`beforebegin`, card.outerHTML);
+  });
 }
 
 loadOffers();
